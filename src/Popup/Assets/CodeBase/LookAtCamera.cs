f3b5ded073fe310b8a0a8be1c5fa8e152ab21ad3
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-01-25 20:44:50 -0500

    Created LookAtCamera script for Popups

---
+++ src/Popup/Assets/CodeBase/LookAtCamera.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace CodeBase
+{
+  public class LookAtCamera : MonoBehaviour
+  {
+    private Camera _mainCamera;
+
+    void Start() => 
+      _mainCamera = Camera.main;
+
+    void Update()
+    {
+      Quaternion rotation = _mainCamera.transform.rotation;
+      transform.LookAt(transform.position + rotation * Vector3.forward, rotation * Vector3.up);
+    }
+  }
+}
