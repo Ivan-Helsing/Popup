149df8fa908b09f5d2724d72dc9d4280347db3e0
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-01-27 00:18:13 -0500

    Extensions created

---
+++ src/Popup/Assets/CodeBase/Extensions/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase.Extensions
+{
+  public static class Extensions
+  {
+    public static void RemoveRedundantListElements(this List<GameObject> list)
+    {
+      if(list.Count <= 3) 
+        return;
+      
+      int startIndex = 2;
+      int redundant = list.Count - startIndex;
+      
+      list.RemoveRange(startIndex, redundant);
+    }
+  }
+}
