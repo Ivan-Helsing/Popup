3c498f4a7ed468a4f969d3c5205a55424a6dcc83
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-01-27 00:19:28 -0500

    AssetProvider created

---
+++ src/Popup/Assets/CodeBase/Services/AssetProvider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace CodeBase.Services
+{
+  public class AssetProvider : IAssetProvider
+  {
+    private const string HintContainerPath = "UI/Hint";
+
+    public GameObject HintContainer() => Load(HintContainerPath);
+
+    private GameObject Load(string path) => Resources.Load<GameObject>(path);
+  }
+}
