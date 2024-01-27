7ba65214b7cddf25b63779a62427a313ba9ef2b3
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-01-26 23:26:07 -0500

    Model created

---
+++ src/Popup/Assets/CodeBase/HintModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase
+{
+  public class HintModel : MonoBehaviour
+  {
+    public Camera MainCamera;
+    private IAssetProvider _assets;
+    private UIFactory _factory;
+
+    private void Start()
+    {
+      _assets = new AssetProvider();
+      _factory = new UIFactory(_assets);
+    }
+
+    private void Update()
+    {
+      if (Input.GetMouseButtonDown(0))
+      {
+        Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit))
+        {
+          if (hit.collider.GetComponent<HintPresenter>() != null)
+          {
+            HintPresenter presenter = hit.collider.GetComponent<HintPresenter>();
+
+            List<GameObject> hintList = presenter.GetComponent<StaticData>().HintList;
+
+            _factory.CreateHint(hintList, presenter.transform);
+          }
+        }
+      }
+    }
+
+  }
+}
