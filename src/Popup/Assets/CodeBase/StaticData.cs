ac675918a7d7ccc8a69529ddfdf5bf644e6824f0
Ivan Helsing <60290995+Ivan-Helsing@users.noreply.github.com>
2024-01-26 23:25:40 -0500

    Static Data created

---
+++ src/Popup/Assets/CodeBase/StaticData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeBase
+{
+  public class StaticData : MonoBehaviour
+  {
+    public List<GameObject> HintList = new();
+    
+    
+  }
+  
+  
+}
